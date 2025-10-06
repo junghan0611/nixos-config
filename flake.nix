95cbcd79c84a4c1b261828789751a26d50dc86f2
junghan0611 <31724164+junghan0611@users.noreply.github.com>
2025-10-07 08:58:31 +0900

    Initial flake configuration for Oracle NixOS

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Junghan's NixOS configurations";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    disko = {
+      url = "github:nix-community/disko/v1.11.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, disko, ... }@inputs: {
+    nixosConfigurations = {
+      # Oracle Cloud ARM VM
+      oracle = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
+        modules = [
+          disko.nixosModules.disko
+          ./hosts/oracle/configuration.nix
+        ];
+      };
+    };
+  };
+}
---
+++ hosts/oracle/vars.nix
@@ -0,0 +1,8 @@
+{
+  hostname = "junghan-oracle-nixos";
+  username = "junghan";
+  sshKey = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEArJDhZ/YWUUZXJjvwAeTp/Y6VOP6acQSTXV8mvrOJyH+yXnEMdasI9MSeVBEhnKW92o94HC7PVkAgMYwzUQqKeRHLROf6AWJHK873pqpuqhSgGfswPQAIQxCq4DEjcio2EDiYZx4thH/4wIcDY2O1IvW10m6sz62vb40R+/zbN819nG6EV0AoYb4aeaOtjWZaQ+XJAW1pBjd/2j9CMBwXyvjodsySIOKC+O2vLwppVg5ZjSdtvN42rfKnszZsAtPibovtsj/8KbeOMKDnsBak11tvrbyZGWZpjPGUawCkEQZi6718G2mZp7qnb4qMCFn0+DkDnQMGPp8tm0zhajYo9w== rsa-key-20220215";
+  locale = "en_US.UTF-8";
+  timezone = "Asia/Seoul";
+}
+
