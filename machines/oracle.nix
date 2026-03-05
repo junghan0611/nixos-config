c11d8f3a45a281e58d1eedb2dd1d3453f411b355
Jung Han <junghanacs@gmail.com>
2026-03-05 22:29:28 +0900

    fix(oracle): nix 관리 명령어 NOPASSWD 추가 (nix-env, nix-collect-garbage, nix-store)

--- machines/oracle.nix
+++ machines/oracle.nix
@@ -141,6 +141,18 @@
           command = "/run/current-system/sw/bin/nixos-rebuild";
           options = [ "NOPASSWD" ];
         }
+        {
+          command = "/run/current-system/sw/bin/nix-env";
+          options = [ "NOPASSWD" ];
+        }
+        {
+          command = "/run/current-system/sw/bin/nix-collect-garbage";
+          options = [ "NOPASSWD" ];
+        }
+        {
+          command = "/run/current-system/sw/bin/nix-store";
+          options = [ "NOPASSWD" ];
+        }
       ];
     }
   ];
